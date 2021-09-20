a3ac910d4e7c31341d3a1b944fc945b42d3288cd
ankithans <ankithans1947@gmail.com>
2021-09-20 16:38:26 +0530

    median of sorted arrays - binary search

--- binary search/4. Median of Two Sorted Arrays.cpp
+++ binary search/4. Median of Two Sorted Arrays.cpp
@@ -30,3 +30,50 @@ public:
 };
 
 // optimized using binary search
+// divide into two parts
+// Input:
+// m = 3, n = 4
+// array1[] = {1,5,9}
+// array2[] = {2,3,6,7}
+// [1,2,3,5||,6,7,9] our ans lies in 4 elements
+
+// 1    |   5,9
+// 2,3,6|   7
+// 1<7 6!<7 low++;
+
+// 1,5  |   9
+// 2,3  |   6,7
+// 5<6 3<9 correct pair; if n+m is odd then ans=max(l1,l2); else (max(l1,l2)+min(r1,r2))/2.0
+class Solution{
+    public:
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+        int n = a.size(), m = b.size();
+        if(n > m) {
+            return findMedianSortedArrays(b, a);
+        }
+        double ans = 0;
+        int sn = (n+m+1)/2;
+        int low = 0, high = n;
+        
+        while(low <= high) {
+            int mid = (low+high)/2;
+
+            int r1 = mid == 0 ? INT_MIN : a[mid-1];
+            int r2 = b[sn-mid-1];
+            
+            int l1 = mid < n ? a[mid] : INT_MAX;
+            int l2 = b[sn-mid];
+            
+            if(r1 <= l2 && r2 <= l1) {
+                if((n+m)%2==1) ans = max(r1, r2);
+                else ans = (max(r1, r2)+min(l1,l2))/2.0;
+                return ans;
+            }
+            
+            if(r1 > l2) high = mid-1;
+            else low = mid+1;
+        }
+        
+        return -1;
+    }
+};
